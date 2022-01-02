2f1abda0fb4fbef95bbf779321e700be38761959
Daniel Wood <daniel.wood253@gmail.com>
2022-01-02 12:00:23 -0800

    changed cat icon to emoji

--- Cats/MainView/MainTBC.swift
+++ Cats/MainView/MainTBC.swift
@@ -21,15 +21,24 @@ class MainTBC: UITabBarController {
     
     func setupTabs() {
         self.viewControllers = [
-        getBarItem(title: "Cats", itemVC: MainVC(), imageName: "star"),
+        getBarItem(title: "😸", itemVC: MainVC(), imageName: nil),
         getBarItem(title: "Options", itemVC: OptionsVC(), imageName: "slider.horizontal.3"),
         ]
         self.selectedIndex = 0
     }
     
-    func getBarItem(title: String, itemVC: UIViewController, imageName: String) -> UIViewController {
-        let itemImage = UIImage(systemName: imageName)?.withRenderingMode(.alwaysTemplate).withTintColor(itemTintColor)
-        let item = UITabBarItem(title: title, image: itemImage, selectedImage: nil)
+    func getBarItem(title: String, itemVC: UIViewController, imageName: String?) -> UIViewController {
+        var itemImage: UIImage?
+        var item : UITabBarItem!
+        
+        if let imageName = imageName {
+            itemImage = UIImage(systemName: imageName)?.withRenderingMode(.alwaysTemplate).withTintColor(itemTintColor)
+            item = UITabBarItem(title: title, image: itemImage, selectedImage: nil)
+        } else {
+            item = UITabBarItem(title: title, image: itemImage, selectedImage: nil)
+            item.setTitleTextAttributes([.font: UIFont.systemFont(ofSize: 30)], for: .normal)
+            item.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -8)
+        }
         itemVC.tabBarItem = item
         return itemVC
     }
